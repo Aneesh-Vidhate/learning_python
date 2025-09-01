7108a75945496bb57b6099b01eeaf54cce387dee
Vidhate Aneesh Rajendra <vidhate.aneesh1947@gmail.com>
2025-09-01 11:08:15 +0530

    Completed "8_strings"

---
+++ 8_strings/add.py
@@ -0,0 +1,21 @@
+introduction = ''' 
+Aneesh Rajendra Vidhate
+21
+Nashik
+'''
+print(introduction)
+
+dish = "Aneesh's favorite dish is Pav Bhaji"
+print(dish)
+
+full_name = 'Aneesh Rajendra Vidhate"Mali"'
+print(full_name)
+
+demo_str = 'Aneesh Rajendra Vidhate'
+print(demo_str[0:10])
+print(demo_str[-1])
+print(demo_str[:])
+print(demo_str[:11])
+print(demo_str[2:-2])
+
+pr
