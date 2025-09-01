af0ca097dc67aa8a2909d62a68ce2d420418de82
Vidhate Aneesh Rajendra <vidhate.aneesh1947@gmail.com>
2025-09-01 18:24:43 +0530

    Completed "10_string_methods"

---
+++ 10_string_methods/add.py
@@ -0,0 +1,8 @@
+str = "Aneesh Rajendra Vidhate"
+print(len(str))
+print(str.upper())
+print(str.lower())
+print(str.isupper())
+print(str.islower())
+print(str.find('j'))
+print(str.replace('j','m'))
