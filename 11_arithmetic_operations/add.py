011c5cb7908372d3551b4027b82c5076c30c5245
Vidhate Aneesh Rajendra <vidhate.aneesh1947@gmail.com>
2025-09-01 18:53:17 +0530

    Completed "11_arithmetic_operations"

---
+++ 11_arithmetic_operations/add.py
@@ -0,0 +1,18 @@
+a = 10 + 20
+print(a)
+b = 10 - 5
+print(b)
+c = 10 * 5
+print(c)
+d = 10 / 3
+print(d)
+e = 10 // 3 #same as division but answer is without decimal point
+print(e)
+f = 10 % 3 #quotient is displayed after division
+print(f)
+g = 10 ** 3 #10 to the power of 3
+print(g)
+a += 3
+print(a)
+a -= 3
+print(a)
