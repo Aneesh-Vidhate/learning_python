0bf824fdc1dff45c01289ac5a7a97235194c6e90
Vidhate Aneesh Rajendra <vidhate.aneesh1947@gmail.com>
2025-09-01 11:07:58 +0530

    Completed "7_exercise3"

---
+++ 7_exercise3/add.py
@@ -0,0 +1,3 @@
+weight_in_pounds = input("Enter the weight in pounds: ")
+weight_in_kilograms = int(weight_in_pounds) * 0.45
+print(weight_in_pounds + " pounds is " + str(weight_in_kilograms) + " kilograms")
